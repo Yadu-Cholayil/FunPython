6782ba138ae843567481190d66cab65d1b8f985f
YaduCholayil <yadu1819@gmail.com>
2022-06-30 13:38:39 +0530

    Guess Game complete

--- guessTheNum.py
+++ guessTheNum.py
@@ -8,12 +8,13 @@ def guess(num,allowedCount):
     while guess != randomNum and count != allowedCount:
         guess = int(input("Guess the number: "))
         count += 1
+        tries = allowedCount - count
         if guess < randomNum:
-            print("Guess again, value is too low")
+            print(f"Guess again, value is too low. You have {tries} tries left!")
         elif guess > randomNum:
-            print("Guess again, value is too high")
+            print(f"Guess again, value is too high. You have {tries} tries left!")
     if guess != randomNum and count == allowedCount:
-        print("Better Luck Next Time")
+        print(f"Better Luck Next Time, the value was {randomNum}.")
     else:
         print(f'Congratulations!!!, You have guessed {randomNum} correctly in {count} tries!!!')
 
@@ -23,16 +24,18 @@ press 1 for Easy \n \
 press 2 for Medium \n \
 press 3 for Hard')
 
-level = int(input())
-if level == 1:
-    print('Easy Difficulty - 1 to 10 \n You have 3 tries')
-    guess(10,3)
-elif level == 2:
-    print('Medium Difficulty - 1 to 40 \n You have 5 tries')
-    guess(40,5)
-elif level ==3:
-    print('Hard Difficulty - 1 to 100 \n You have 10 tries')
-    guess(100,10)
-else:
-    print("Please make a valid selection")
-
+try:
+    level = int(input())
+    if level == 1:
+        print('Easy Difficulty - 1 to 10 \n You have 3 Guesses.')
+        guess(10,3)
+    elif level == 2:
+        print('Medium Difficulty - 1 to 40 \n You have 5 Guesses.')
+        guess(40,5)
+    elif level ==3:
+        print('Hard Difficulty - 1 to 100 \n You have 5 Guesses.')
+        guess(100,5)
+    else:
+        print("Please make a valid selection.")
+except:
+    print("Enter a valid input")
