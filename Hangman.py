7ea42d256e10ae40f273f767d914acef6ee87562
YaduCholayil <yadu1819@gmail.com>
2022-07-03 15:54:27 +0530

    Hangman Done

--- Hangman.py
+++ Hangman.py
@@ -10,6 +10,47 @@
 
 import random
 from words import words
+import string
 
-word = random.choice(words)
-print(word)
+def validWord():
+    word = random.choice(words) # Chooses a random word
+    # while "-" not in word or " " not in word:
+    #     word = random.choice(words)
+    #     print(word)
+    return word.upper()
+
+def hangman():
+    word = validWord()
+    wordLetters = set(word) # Store letters in a word as a set - avoids duplicates
+    alphabet = set(string.ascii_uppercase)
+    # User choosed letters
+    usedLetters = set()
+    
+    lives = 7
+    # User input a letter
+    while len(wordLetters) > 0 and lives > 0:
+        print(f"lives: {lives}")
+        #show the used letters
+        print('You have used the letters: ' + ' '.join(usedLetters))
+
+        #show the Word to User
+        wordList = [letter if letter in usedLetters else '-' for letter in word]
+        print('Current word: ' + " ".join(wordList))
+
+        userLetter = input("Enter your Guess: ").upper()
+        if userLetter in alphabet - usedLetters:
+            usedLetters.add(userLetter)
+            if userLetter in wordLetters:
+                wordLetters.remove(userLetter)
+            else:
+                lives -= 1
+                print("Letter not in the word")
+        elif userLetter in usedLetters:
+            print("You have already entered that character. Please enter another character")
+        else:
+            print("Enter a Valid character")
+    if lives == 0 :
+        print(f'You have died. The word was {word}')
+    else:
+        print(f"Congradulations!!! The word is {word}!!!!!")
+hangman()
